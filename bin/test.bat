036bb4455752c66e1748d99e89e026aaa911cf00
Linudows <103470728+Linudows@users.noreply.github.com>
2022-04-18 11:06:28 +0800

    reg pgsql as win serv

--- bin/test.bat
+++ bin/test.bat
@@ -2,7 +2,8 @@ initdb.exe -D ../testdb
 echo host	all	all	0.0.0.0/0	trust  >>  ../testdb/pg_hba.conf
 echo shared_preload_libraries = 'citus'  >>  ../testdb/postgresql.conf
 pause
-pg_ctl -D ../testdb -l ../testdb_logfile.txt start
+pg_ctl register -N "pgsql-testdb" -D ../testdb
+net start pgsql-testdb
 pause
 psql.exe -c "CREATE EXTENSION citus;" postgres
 psql.exe -c "select citus_version();" postgres
